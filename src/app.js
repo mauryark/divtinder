aa7bce5822f42712e0de65e4ac914d718194ebfc
Raj <herajmaurya@gail.com>
2025-05-18 21:40:13 +0530

    initial changes

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app =  express();
+
+
+app.use('/', (req, res) => {
+  res.send('Hello from Dashboard!');
+});
+app.use('/server', (req, res) => {
+  res.send('Hello from server!');
+});
+
+
+app.listen(3000, ()=>{
+  console.log("Server is successfully listing!");
+});
