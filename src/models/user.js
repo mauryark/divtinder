d8ab84c1d3cee6e905d0f2f82cfd80d49f82c92b
Raj <herajmaurya@gail.com>
2025-05-23 18:24:00 +0530

    adding validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: {type:String},
-  lastName: {type:String},
-  email: {type:String},
-  password: {type:String},
-  age: {type:Number},
-  gender: {type:String}
+  firstName: {
+    type: String,
+    require: true,
+    minLength: 4,
+    maxLength: 50,
+    trim: true,
+  },
+  lastName: { type: String, trim: true },
+  email: { type: String, unique: true, trim: true, lowercase: true },
+  password: { type: String },
+  age: { type: Number, min: 18 },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "other"].includes(value)) {
+        throw new Error("Gender data is not valid!");
+      }
+    },
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
